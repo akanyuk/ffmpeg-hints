f17dd608cadc0f46ebdda9bf56ab83a6d8ea851e
marinov <marinov@sedmax.ru>
2025-07-02 16:37:00 +0300

    Conversion script for OpenMSX added

--- linux/convert-atari800.sh
+++ linux/convert-atari800.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
 
-# -an - no sound
-ffmpeg -ss 00:00:02.9 -t 00:01:00.0 -i $1 -vf "scale=1680:1200:flags=neighbor,crop=1680:1080:0:60" -an $1.mp4
+# -an            - no sound
+# -ss 00:00:02.9 - start
+# -t 00:01:00.0  - duration
+
+ffmpeg -i $1 -vf "scale=1680:1200:flags=neighbor,crop=1680:1080:0:60" $1.mp4
--- linux/convert-zx.sh
+++ linux/convert-zx.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
 
-# -an - no sound
-ffmpeg -ss 00:00:00.000 -t 00:01:00.0 -i $1 -vf "scale=1536:1152:flags=neighbor,crop=1536:1080:0:36" $1.mp4
+# -an            - no sound
+# -ss 00:00:02.9 - start
+# -t 00:01:00.0  - duration
+
+ffmpeg -i $1 -vf "scale=1536:1152:flags=neighbor,crop=1536:1080:0:36" $1.mp4

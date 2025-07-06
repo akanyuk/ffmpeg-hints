aea72aa353b1f1384864bc3d3bf7a3ec69ebc1e2
nyuk <aka.nyuk@gmail.com>
2025-07-06 20:00:08 +0300

    dosbox an zx converters updated

--- linux/convert-dosbox-640x480.sh
+++ linux/convert-dosbox-640x480.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
 
-# -an - no sound
-ffmpeg -ss 00:00:00.000 -t 00:01:00.0 -an -i $1 -vf "scale=1920:1440:flags=neighbor" $1.mp4
+# -an            - no sound
+# -ss 00:00:02.9 - start
+# -t 00:01:00.0  - duration
+
+ffmpeg -i $1 -vf "scale=1920:1440:flags=neighbor" $1.mp4
